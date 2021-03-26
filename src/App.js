7bf0e322d42289af71ae9451808abe6eb3aa6c49
Karol Cislowski <cislowski.karol@hotmail.com>
2021-03-26 19:12:52 +0100

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  // What we need to do?
+  // We need to have possibility to show:
+  // 1. List of ice creams options
+  // 2. Single ice cream with id provided by url
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>nothing here... YET</p>
+  )
 }
 
-export default App;
+export default App
